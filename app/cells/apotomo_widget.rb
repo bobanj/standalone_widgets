c30b521a31371f1a21d6da65900fc7a06e82e58d
Boban <bobanj@gmail.com>
2010-12-23 11:34:11 +0100

    independent widgets

--- app/cells/apotomo_widget.rb
+++ app/cells/apotomo_widget.rb
@@ -7,6 +7,9 @@ class ApotomoWidget < Apotomo::Widget
   def update
     # TODO render :json doesent work - requests template
     # TODO using :callback param when rendering text doesent work
+    puts "@@@@@@@@@@@@@@2"
+    p params
+    puts "@@@@@@@@@@@"
     text = params[:callback] + '(' + PostLink.all.to_json + ')'
     render :text => text, :content_type => 'application/json'
   end
